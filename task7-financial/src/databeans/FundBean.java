d5a1e3ab636894eb73d3e988aede9ea1397163d4
suhmily <suhmily@gmail.com>
2015-01-19 16:03:23 -0500

    Update FundBean.java

--- task7-financial/src/databeans/FundBean.java
+++ task7-financial/src/databeans/FundBean.java
@@ -7,10 +7,10 @@ public class FundBean {
 	private String symbol;
 	
 	public int    getId()                { return id;           }
-  public String getName()              { return name;         }
-  public String getSymbol()            { return symbol;    }
+	public String getName()              { return name;         }
+  	public String getSymbol()            { return symbol;    }
     
-  public void   setId(int i)           { id = i;              }
+  	public void   setId(int i)           { id = i;              }
 	public void   setName(String s)      { name = s;            }
 	public void   setSymbol(String s)    { symbol = s;       }
 
