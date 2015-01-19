122406b120899622f12d5f40c2f4323f1dbe228b
ranz92 <zranz.92@gmail.com>
2015-01-19 15:56:06 -0500

    databean

--- task7-financial/src/databeans/CustomerBean.java
+++ task7-financial/src/databeans/CustomerBean.java
@@ -1,5 +1,84 @@
 package databeans;
 
+//@PrimaryKey("customerId")
 public class CustomerBean {
+	private int customerId;
+	private String username;
+	private String password;
+	private String fName;
+	private String lName;
+	private String addrL1;
+	private String addrL2;
+	private String city;
+	private String state;
+	private int zip;
+	private long cash;
+	
+	public int getCustomerId() {
+		return customerId;           
+	}
+	public String getUsername() {
+		return username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public String getFname() {
+		return fName;
+	}
+	public String getLname() {
+		return lName;
+	}
+	public String getAddrL1() {
+		return addrL1;
+	}
+	public String getAddrL2() {
+		return addrL2;
+	}
+	public String getCity() {
+		return city;
+	}
+	public String getState() {
+		return state;
+	}
+	public int getZip() {
+		return zip;
+	}
+	public long getCash() {
+		return cash;
+	}
 
+    public void setCustomerId(int i) { 
+    	customerId = i;     
+    }
+    public void setUsername(String s) {
+    	username = s;
+    }
+    public void setPassword(String s) {
+    	password = s;
+    }
+    public void setFname(String s) {
+    	fName = s;
+    }
+    public void setLname(String s) {
+    	lName = s;
+    }
+    public void setAddrL1(String s) {
+    	addrL1 = s;
+    }
+    public void setAddrL2(String s) {
+    	addrL2 = s;
+    }
+    public void setCity(String s) {
+    	city = s;
+    }
+    public void setState(String s) {
+    	state = s;
+    }
+    public void setZip(int i) {
+    	zip = i;
+    }
+    public void setCash(long l) {
+    	cash = l;
+    }
 }
--- task7-financial/src/databeans/EmployeeBean.java
+++ task7-financial/src/databeans/EmployeeBean.java
@@ -1,5 +1,34 @@
 package databeans;
-
+//@PrimaryKey("username")
 public class EmployeeBean {
-
+	private String username;
+	private String password;
+	private String fName;
+	private String lName;
+	
+	public String getUsername() {
+		return username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public String getFname() {
+		return fName;
+	}
+	public String getLname() {
+		return lName;
+	}
+	
+	public void setUsername(String s) {
+	    username = s;
+	}
+	public void setPassword(String s) {
+    	password = s;	 
+    }
+	public void setFname(String s) {
+    	fName = s;
+    }
+	public void setLname(String s) {
+	   	lName = s;
+    }
 }
