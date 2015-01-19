8978d6d72fb21bc645a4f919aff17a1954465182
Jiayi Zhu <quriola@gmail.com>
2015-01-19 16:14:20 -0500

    Edited PriceBean

--- task7-financial/src/databeans/PriceBean.java
+++ task7-financial/src/databeans/PriceBean.java
@@ -11,16 +11,16 @@ public class PriceBean {
 	Date price_date;
 	long price;
 	
-	public int getFund_id() {
+	public int getFundId() {
 		return fund_id;
 	}
-	public void setFund_id(int fund_id) {
+	public void setFundId(int fund_id) {
 		this.fund_id = fund_id;
 	}
-	public Date getPrice_date() {
+	public Date getPriceDate() {
 		return price_date;
 	}
-	public void setPrice_date(Date price_date) {
+	public void setPriceDate(Date price_date) {
 		this.price_date = price_date;
 	}
 	public long getPrice() {
