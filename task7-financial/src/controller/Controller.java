ecac4bbf980fb87e8ef55f7867c361742dc9576f
ranz92 <zranz.92@gmail.com>
2015-01-21 19:31:47 -0500

    updated

--- task7-financial/src/controller/Controller.java
+++ task7-financial/src/controller/Controller.java
@@ -95,7 +95,7 @@ public class Controller extends HttpServlet {
     	}
 
     	if (nextPage.endsWith(".jsp")) {
-//	   		RequestDispatcher d = request.getRequestDispatcher("WEB-INF/" + nextPage);
+	//   		RequestDispatcher d = request.getRequestDispatcher("WEB-INF/" + nextPage);
     		RequestDispatcher d = request.getRequestDispatcher(nextPage);
 	   		d.forward(request,response);
 	   		return;
